ddf67a2f1050f8c63ecd48c872ecea956dad9f95
Thabiso Mathebula <thabiso@khoyo.com>
2017-01-25 22:41:34 +0200

    Added Unit Tests for non-void methods

---
+++ TangentTests/Helpers/DALTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tangent.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tangent.Models;
+
+namespace Tangent.Helpers.Tests
+{
+    [TestClass()]
+    public class DALTests
+    {
+        public static string token = "b7ec34e136bb6d28a4421e422e852b99cc834d17";
+
+        [TestMethod()]
+        public void GetProjectsTest()
+        {
+            IEnumerable<Project> projects = DAL.GetProjects(token);
+
+            Assert.IsNotNull(projects);
+        }
+
+
+        [TestMethod()]
+        public void GetProjectByIdTest()
+        {
+            string projectTItle = "Stark Industries CRM";
+
+            Project project = DAL.GetProjectById(token, 61);
+
+            Assert.AreEqual(projectTItle, project.title);
+        }
+
+        [TestMethod()]
+        public void LoginTest_Success()
+        {
+            string actual = DAL.Login(new User() { username = "admin1", password = "admin1" }).token;
+            string expected = "b7ec34e136bb6d28a4421e422e852b99cc834d17";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
