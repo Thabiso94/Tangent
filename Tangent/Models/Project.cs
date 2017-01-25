5bcb2ff5f6a801d59609b995c9dd98959941d823
Thabiso Mathebula <thabiso@khoyo.com>
2017-01-25 07:53:55 +0200

    Working on styling the application

--- Tangent/Models/Project.cs
+++ Tangent/Models/Project.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Tangent.Models
 {
@@ -10,11 +11,23 @@ namespace Tangent.Models
         static HttpClient client = new HttpClient();
 
         public int pk { get; set; }
+
+        [DisplayName("Title")]
         public string title { get; set; }
+
+        [DisplayName("Description")]
         public string description { get; set; }
+
+        [DisplayName("Start Date")]
         public string start_date { get; set; }
+
+        [DisplayName("End Date")]
         public string end_date { get; set; }
+
+        [DisplayName("Billable")]
         public bool is_billable { get; set; }
+
+        [DisplayName("Active")]
         public bool is_active { get; set; }
         public List<TaskSet> task_set { get; set; }
         public List<object> resource_set { get; set; }
@@ -35,20 +48,40 @@ namespace Tangent.Models
     public class ProjectData
     {
         public int pk { get; set; }
+
+        [DisplayName("Title")]
         public string title { get; set; }
+
+        [DisplayName("Description")]
         public string description { get; set; }
+
+        [DisplayName("Start Date")]
         public string start_date { get; set; }
+
+        [DisplayName("End Date")]
         public object end_date { get; set; }
+
+        [DisplayName("Billable")]
         public bool is_billable { get; set; }
+
+        [DisplayName("Active")]
         public bool is_active { get; set; }
     }
 
     public class TaskSet
     {
         public int id { get; set; }
+
+        [DisplayName("Title")]
         public string title { get; set; }
+
+        [DisplayName("Due")]
         public string due_date { get; set; }
+
+        [DisplayName("Estimated Hours")]
         public string estimated_hours { get; set; }
+
+        [DisplayName("Project")]
         public int project { get; set; }
         public ProjectData project_data { get; set; }
     }
--- Tangent/Models/User.cs
+++ Tangent/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,17 +8,33 @@ namespace Tangent.Models
 {
     public class User
     {
+        [DisplayName("Password")]
         public string password { get; set; }
+
         public int id { get; set; }
+
+        [DisplayName("First Name")]
         public string first_name { get; set; }
+
+        [DisplayName("Last Name")]
         public string last_name { get; set; }
+
+        [DisplayName("Username")]
         public string username { get; set; }
+
+        [DisplayName("Email")]
         public string email { get; set; }
+
+        [DisplayName("Staff")]
         public bool is_staff { get; set; }
+
+        [DisplayName("Super User")]
         public bool is_superuser { get; set; }
         public Profile profile { get; set; }
         public List<object> authentications { get; set; }
         public List<object> roles { get; set; }
+
+        [DisplayName("Token")]
         public string token { get; set; }
     }
 
